b653f429f85162e855cfed14b9f35ff13edcc187
Antonyj26 <antonyjosu26@gmail.com>
2025-06-12 17:01:38 -0300

    Updating Products

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -6,7 +6,7 @@ const productsRoutes = Router();
 const productsController = new ProductController();
 
 productsRoutes.get("/", productsController.index);
-
 productsRoutes.post("/", productsController.create);
+productsRoutes.put("/:id", productsController.update);
 
 export { productsRoutes };
