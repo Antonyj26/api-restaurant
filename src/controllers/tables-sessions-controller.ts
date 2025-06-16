f1e6eebf55fd1e7664138d4a3b4d058c6bc4a2b8
Antonyj26 <antonyjosu26@gmail.com>
2025-06-16 15:27:14 -0300

    Listing Table Sessions

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -33,6 +33,18 @@ class TablesSessionsController {
       next(error);
     }
   }
+
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const sessions = await knex<TableSessionRepository>(
+        "tables_sessions"
+      ).orderBy("closed_at");
+
+      return response.json(sessions);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export { TablesSessionsController };
