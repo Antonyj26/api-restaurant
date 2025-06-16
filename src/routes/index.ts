41ea06892c452f6cbf669b3e1f42f466c7bef659
Antonyj26 <antonyjosu26@gmail.com>
2025-06-16 14:13:26 -0300

    Route and Table Session Controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { productsRoutes } from "./products-routes";
 import { tablesRoutes } from "./tables-routes";
+import { tablesSessionsRoutes } from "./table-sessions-routes";
 
 const routes = Router();
 
 routes.use("/tables", tablesRoutes);
 routes.use("/products", productsRoutes);
+routes.use("/tables-sessions", tablesSessionsRoutes);
 
 export { routes };
