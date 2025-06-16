b8221ede245dcde7faccd3ec59a85d5e4be0bc98
Antonyj26 <antonyjosu26@gmail.com>
2025-06-16 13:20:13 -0300

    Listing tables

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-
 import { productsRoutes } from "./products-routes";
+import { tablesRoutes } from "./tables-routes";
 
 const routes = Router();
 
+routes.use("/tables", tablesRoutes);
 routes.use("/products", productsRoutes);
 
 export { routes };
